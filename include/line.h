abaab654e24e1d89f550c60c70bb864edbff33c9
BlackJackMC <duytu85343@gmail.com>
2024-08-24 23:52:08 +0700

    Add QTR sensor

---
+++ include/line.h
@@ -0,0 +1,33 @@
+#ifndef LINE_H
+#define LINE_H
+
+#include <Arduino.h>
+#include <QTRSensors.h>
+
+namespace line
+{
+    QTRSensors sensor;
+    bool enabled = false;
+
+    const int count = 5;
+    const uint8_t pin[] = {8, 9, 10, 11, 12};
+    int value[5];
+
+    void setup()
+    {
+        sensor.setTypeRC();
+        sensor.setSensorPins(pin, count);
+
+        digitalWrite(LED_BUILTIN, HIGH);
+
+        for (int i = 0; i < 400; i++)
+        {
+            sensor.calibrate();
+        }
+
+        digitalWrite(LED_BUILTIN, LOW);
+        enabled = true;
+    }
+}
+
+#endif
