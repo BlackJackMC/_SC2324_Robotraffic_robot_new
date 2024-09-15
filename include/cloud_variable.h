8b9d356f4e8ba94926ea6fbb480bd9d4848b4a6f
BlackJackMC <duytu85343@gmail.com>
2024-09-15 23:08:02 +0700

    Found the problem in update function of cloud_variable, will fix soon

--- include/cloud_variable.h
+++ include/cloud_variable.h
@@ -4,55 +4,24 @@
 #include <ArduinoJson.h>
 #include "mqtt.h"
 
-using namespace mqtt;
-
 template <typename T>
 class CloudVar
 {
 public:
     const String topic = "variable/";
 
-    CloudVar(){}
-    CloudVar(String name): name(topic + name)
-    {
-        update();
-    }
-    CloudVar(String name, T init_val): name(topic + name), value(init_val)
-    {
-        update();
-    }
-    CloudVar(String name, T init_val, std::function<void(void)> callback): name(topic + name), value(init_val)
-    {
-        this->callback = callback;
-        update();
-    }
+    CloudVar();
+    CloudVar(String name);
+    CloudVar(String name, T init_val);
+    CloudVar(String name, T init_val, std::function<void(void)> callback);
 
-    void set_name(String name)
-    {
-        this->name = topic + name;
-    }
-    void set_initial_value(T init_val)
-    {
-        value = init_val;
-    }
-    void set_callback(std::function<void(void)> callback)
-    {
-        this->callback = callback;
-    }
+    void set_name(String name);
+    void set_initial_value(T init_val);
+    void set_callback(std::function<void(void)> callback);
 
-    T get()
-    {
-        return value;
-    }
-    void set(T new_val)
-    {
-        value = new_val;
-        mqtt::client.publish(name.c_str(), String(new_val).c_str());
-    }
-    void operator=(T new_val)
-    {
-        set(new_val);
-    }
+    T get();
+    void set(T new_val);
+    void operator=(T new_val);
 
     String name;
 
@@ -60,12 +29,80 @@ private:
     T value;
     std::function<void(void)> callback;
 
-    void update()
-    {
-        mqtt::on(name.c_str(), [this](String message) {
-            JsonDocument temp; //Life hack
-            temp["life hack"] = message;
-            value = temp["life hack"].as<T>();
-        });
-    }
+    void update();
 };
+
+// Template Definitions
+
+template <typename T>
+CloudVar<T>::CloudVar() {}
+
+template <typename T>
+CloudVar<T>::CloudVar(String name) : name(topic + name), value()
+{
+    update();
+}
+
+template <typename T>
+CloudVar<T>::CloudVar(String name, T init_val) : name(topic + name), value(init_val)
+{
+    update();
+}
+
+template <typename T>
+CloudVar<T>::CloudVar(String name, T init_val, std::function<void(void)> callback) : name(topic + name), value(init_val), callback(callback)
+{
+    update();
+}
+
+template <typename T>
+void CloudVar<T>::set_name(String name)
+{
+    this->name = topic + name;
+}
+
+template <typename T>
+void CloudVar<T>::set_initial_value(T init_val)
+{
+    value = init_val;
+}
+
+template <typename T>
+void CloudVar<T>::set_callback(std::function<void(void)> callback)
+{
+    this->callback = callback;
+}
+
+template <typename T>
+T CloudVar<T>::get()
+{
+    return value;
+}
+
+template <typename T>
+void CloudVar<T>::set(T new_val)
+{
+    value = new_val;
+    mqtt::client.publish(name.c_str(), String(new_val).c_str());
+}
+
+template <typename T>
+void CloudVar<T>::operator=(T new_val)
+{
+    set(new_val);
+}
+
+template <typename T>
+void CloudVar<T>::update()
+{
+    // mqtt::on(name.c_str(), [this](String message) {
+    //     // Use DynamicJsonDocument for dynamic allocation
+    //     DynamicJsonDocument temp(1024);
+    //     temp["value"] = message;
+    //     value = temp["value"].as<T>();
+    //     if (callback)
+    //     {
+    //         callback();
+    //     }
+    // });
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <map>
 
 #include "net.h"
 #include "mqtt.h"
@@ -9,30 +8,34 @@
 
 void setup()
 {
-    Serial.begin(115200);
-    while (!Serial)
-        ; // Wait for serial
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("Serial");
-    net::setup();
-    mqtt::client.publish("serial", "connected to internet");
-    mqtt::setup();
-    mqtt::client.publish("serial", "network setup completed");
-    setup_sequence::setup();
-    digitalWrite(LED_BUILTIN, LOW);
+    // digitalWrite(LED_BUILTIN, HIGH);
+    // Serial.begin(115200);
+    // while (!Serial)
+    //     ; // Wait for serial
+    // Serial.println("Serial");
+    // net::setup();
+    // mqtt::client.publish("serial", "connected to internet");
+    // mqtt::setup();
+    // mqtt::client.publish("serial", "network setup completed");
+    // setup_sequence::setup();
+    // digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
-    if (!mqtt::client.connected())
-        mqtt::connect();
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+    // if (!mqtt::client.connected())
+    //     mqtt::connect();
 
-    mqtt::client.loop();
-    if (parameter::can_go.get())
-    {
-        parameter::update_angle();
-        motor::go(parameter::speed.get(), parameter::direction.get());
-    }
-    else
-        motor::stop();
+    // mqtt::client.loop();
+    // if (parameter::can_go.get())
+    // {
+    //     parameter::update_angle();
+    //     motor::go(parameter::speed.get(), parameter::direction.get());
+    // }
+    // else
+    //     motor::stop();
 }
